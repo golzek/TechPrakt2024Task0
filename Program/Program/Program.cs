634c70d3b5f7fec04d80fc6380dcdae72aec19f8
Євген Головня <vt221_gyem@student.ztu.edu.ua>
2024-06-09 18:09:29 +0300

    TechPrakt2024Task0

---
+++ Program/Program/Program.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+Console.OutputEncoding=Encoding.UTF8;
+Console.Write("Введіть число:");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введіть число:");
+double b = Convert.ToDouble(Console.ReadLine());
+
+double sum = a + b;
+Console.WriteLine($"Сума чисел:{sum}");
